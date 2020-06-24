f32a738b963e1c2dc6c41bc7a60d5e98b37389e1
yabouyou <33936354+yabouyou@users.noreply.github.com>
2020-06-24 12:49:12 -0400

    Add files via upload

---
+++ PageParser.py
@@ -0,0 +1,193 @@
+from bs4 import BeautifulSoup as bs
+import requests
+import urllib
+import concurrent.futures
+import time
+import re
+import random
+import os
+import asyncio
+import aiohttp
+import aiofiles
+
+from typing import List
+
+
+class wikiPerson:
+    # static variable so that urls aren't repeated
+    urls = []
+    img_urls = []
+    img_names = []
+    sentences = []
+
+    def __init__(self, url, height):
+        self.url = url
+        self.name = None
+        self.img_path = None
+        self.intro_sentence = None
+        self.children = []
+        self.img_urls = []
+        self.__class__.urls.append(self.url)
+
+        if height:
+            self.set_child_vars(height)
+
+            # downloads asynchronously to speed up run time
+            self.download_all()
+
+    def download_all(self):
+
+       for i in range(len(self.__class__.img_urls)):
+           self.download_image(self.__class__.img_urls[i], self.__class__.img_names[i])
+
+    def download_image(self, url, name):
+        # download the thumbnail image
+        img = requests.get(url).content
+        with open(name, 'wb') as img_file:
+            img_file.write(img)
+
+    def set_child_vars(self, height):
+        if height >= 0:
+            self.set_vars(height == 0)
+            for child in self.children:
+                child.set_child_vars(height - 1)
+
+    def set_vars(self, is_leaf):
+        res = requests.get(self.url)
+        soup = bs(res.content, 'lxml')
+
+        children = []
+
+        # only includes people who have articles with first and last names
+        # i.e. the musician "Prince" would NOT be included
+
+        # set the name
+        self.name = soup.title.string.split("-")[0][:-1]
+
+        # set the image link
+        table = soup.find('table', class_='infobox biography vcard')
+
+        if not table:
+            table = soup.find('table', class_='infobox vcard')
+
+        # safe operation since we checked that an image exists when creating the Person object
+        img = table.find("img")
+        img_link = "http:" + img.get("src")
+        self.__class__.img_urls.append(img_link)
+
+        # images are stored in images folder in cwd
+        self.img_path = os.path.abspath(os.path.join(os.getcwd(), "images", self.name.replace(" ", "_") + ".GIF"))
+        self.__class__.img_names.append(self.img_path)
+
+        if not is_leaf:
+            # set the child links
+            # put the links through a "soft filter" i.e. checking if link title fits format of name
+            # then through a "hard filter", i.e. checking if it contains a biography tag in the html
+            soup_links = soup.find("div", {"id": "bodyContent"}).find_all("a")
+            possible_names = []
+            i = 0
+
+            for link in soup_links:
+                new = link.get("href", "")
+                curr_url = "https://en.wikipedia.org" + new
+
+                # ensure person hasn't already been added
+                if wikiPerson.is_name_link(new) and curr_url not in self.__class__.urls:
+                    possible_names.append(new)
+
+            people_added = 0
+            i = 0
+
+            paragraphs = soup.select("p")
+
+            while i < len(possible_names) and people_added < 2:
+                with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
+                    f1 = executor.submit(wikiPerson.is_person, possible_names[i])
+                    if f1.result():
+                        new_person = wikiPerson("https://en.wikipedia.org" + possible_names[i], 0)
+                        tmp_name = possible_names[i][6:].split("_")
+                        name = tmp_name[0] + " " + tmp_name[1]
+                        new_person.intro_sentence = wikiPerson.get_sentence(paragraphs, name)
+                        children.append(new_person)
+                        people_added += 1
+                    i += 1
+
+            self.children = children
+
+    def get_path(self):
+        return self.img_path
+
+    def get_name(self):
+        return self.name
+
+    def get_intro(self):
+        return self.intro_sentence
+
+    @staticmethod
+    def get_sentence(paragraphs, name):
+        for para in paragraphs:
+            if name in para.text:
+                sentences = para.text.split(".")
+                for sentence in sentences:
+                    if name in sentence:
+                        return sentence + "."
+
+    def get_bfs_paths(self):
+        '''
+        :return: a 3-tuple of containing 3 lists, one containing paths, one containing sentences and the other containing names
+         of the images stored in tree in BFS order
+        '''
+        paths = []
+        names = []
+        intro_sents = []
+
+        # start with root node
+        queue = [self]
+        while queue:
+            curr_node = queue.pop(0)
+            paths.append(curr_node.get_path())
+            names.append(curr_node.get_name())
+            intro_sents.append(curr_node.get_intro())
+            queue.extend(curr_node.children)
+
+        return tuple([paths, intro_sents[1:], names])
+
+    @staticmethod
+    def is_person(new):
+        """
+        :param new: a wikipedia url that starts with /wiki/
+        :return: returns whether is a link to a wikipedia page about a person
+        """
+        if new.startswith("/wiki/"):
+            is_person = False
+            new_url = "https://en.wikipedia.org" + new
+            new_soup = bs(requests.get(new_url).text, 'lxml')
+            # only articles about people have the 'infobox biography vcard tag';
+            # this distinguishes from other articles that match the regex
+            # less notable people won't have the tag either
+            table = new_soup.find('table')
+
+            if not table:
+                return False
+
+            rows = table.find_all('tr')
+            has_img = table.find('img') is not None
+
+            for row in rows:
+                curr = row.find('th')
+                # 'born' header only for articles about people
+                if curr and 'Born' in curr.text:
+                    is_person = True
+
+            if is_person and has_img:
+                return True
+            return False
+
+    @staticmethod
+    def is_name_link(new):
+        """
+        :param new: A wikipedia url that starts with /wiki/
+        :return: returns true if url contains two words and both words contain capital letters
+        """
+        parsed_link = new[6:].split("_")
+        return len(parsed_link) == 2 and all(re.search("^[A-Z]", elem) for elem in parsed_link)
---
+++ TreeVisualizer.py
@@ -0,0 +1,236 @@
+from tkinter import *
+from PIL import Image, ImageTk
+import time
+import random
+import math
+from PageParser import wikiPerson
+import numpy as np
+
+start = time.perf_counter()
+
+class ImageTree:
+    # static variable for convenient lookup of nodes
+    instances = []
+
+    def __init__(self, shape, tag, name_obj, total, canvas, sentences):
+        self.__class__.instances.append(self)
+        self.shape = shape
+        self.tag = tag
+        self.name_obj = name_obj
+        self.total = total
+        self.canvas = canvas
+        self.sentences = []
+        self.sentence = None
+        self.expanded = False
+        self.children = []
+        self.edges = []
+
+        # parse tag to get the index of node
+        node_num = int(tag.split('_')[1])
+
+        if node_num:
+            self.sentence = sentences[node_num - 1]
+
+        # left and right child indices for shape number
+        left_i = 2 * node_num + 1
+        right_i = 2 * node_num + 2
+
+        left_tag = "shape_" + str(left_i)
+        right_tag = "shape_" + str(right_i)
+        left_name_tag = "name_" + str(left_i)
+        right_name_tag = "name_" + str(right_i)
+
+        if right_i < total:
+            left_node = canvas.find_withtag(left_tag)
+            right_node = canvas.find_withtag(right_tag)
+
+            left_name = canvas.find_withtag(left_name_tag)
+            right_name = canvas.find_withtag(right_name_tag)
+
+            # recursive call to make tree with tags as values
+            self.children.append(ImageTree(left_node, left_tag, left_name, total, canvas, sentences))
+            self.children.append(ImageTree(right_node, right_tag, right_name, total, canvas, sentences))
+
+        elif left_i < total:
+            left_node = canvas.find_withtag(left_tag)
+            self.children.append(ImageTree(left_node, left_tag, left_name_tag, canvas, total))
+
+    def expand(self):
+        if self.children:
+            for child in self.children:
+                canvas = self.canvas
+                child_coords = canvas.coords(child.shape)
+                parent_coords = canvas.coords(self.shape)
+
+                # change temporary parent coordinates to point underneath shape
+                if self.edges:
+                    parent_coords[0] += 50
+                    parent_coords[1] += 80
+                else:
+                    parent_coords[0] -= 10
+                    parent_coords[1] += 80
+
+
+                # change temporary child coordinates to point above shape
+                child_coords[0] += 25
+                child_coords[1] -= 5
+
+                distance = math.sqrt(
+                    ((parent_coords[0] - child_coords[0]) ** 2) + ((parent_coords[1] - child_coords[1]) ** 2))
+
+                sent_coords = [(child_coords[0] + parent_coords[0]) / 2, (child_coords[1] + parent_coords[1]) / 2]
+
+
+                padding = 25
+                if self.edges:
+                    new_sent = canvas.create_text(sent_coords[0] + 35, sent_coords[1] - padding * 2,
+                                                  text=child.sentence, width=distance,
+                                                  tags='sent_' + str(self.total), font=("Times New Roman", 8),
+                                                  fill='white')
+                else:
+                    new_sent = canvas.create_text(sent_coords[0] - 35, sent_coords[1] - padding * 2,
+                                                  text=child.sentence, width=distance,
+                                                  tags='sent_' + str(self.total), font=("Times New Roman", 8),
+                                                  fill='white')
+
+                # keep track of edges and sentences placed so I can delete them later
+                self.edges.append(canvas.create_line(child_coords, parent_coords, fill='white'))
+                self.sentences.append(new_sent)
+
+                # change child nodes to become visible, i.e. expand tree
+                canvas.itemconfig(child.shape, state='normal')
+                canvas.itemconfig(child.name_obj, state="normal")
+
+            self.expanded = True
+
+    def collapse(self):
+        # recursively collapse all children and sub-children
+        if self.children:
+            for child in self.children:
+                child_tag = child.get_tag()
+                child_shape = canvas.find_withtag(child_tag)
+                # change child nodes to become hidden
+                canvas.itemconfig(child_shape, state='hidden')
+                canvas.itemconfig(child.name_obj, state="hidden")
+                child.collapse()
+
+            for edge in self.edges:
+                canvas.delete(edge)
+
+            self.edges = []
+
+            for sentence in self.sentences:
+                canvas.delete(sentence)
+
+            self.expanded = False
+
+    def get_tag(self):
+        return self.tag
+
+    def is_expanded(self):
+        return self.expanded
+
+    @classmethod
+    def get_node_with(cls, tag):
+        for ins in cls.instances:
+            if ins.get_tag() == tag:
+                return ins
+
+
+def click(event):
+    q = event.widget.find_withtag("current")
+    canvas = event.widget
+    tag = canvas.gettags(q)[0]
+    node = ImageTree.get_node_with(tag)
+
+    # ensures only visible images can be expanded
+    if node and canvas.itemcget(q, 'state') == "normal":
+        if node.is_expanded():
+            node.collapse()
+        else:
+            node.expand()
+
+# initialize window
+def init_gui(URL):
+    tk = Tk()
+    WINDOW_HEIGHT = 1000
+    WINDOW_WIDTH = 1200
+    canvas = Canvas(tk, width=WINDOW_WIDTH, height=WINDOW_HEIGHT)
+    canvas.configure(background="#181a1b")
+    tk.title("Wikipedia Tree Visualizer")
+    canvas.pack()
+
+    root = None
+    root_name = None
+    tree_height = 2
+    width = WINDOW_WIDTH
+    height = 10
+    i = 2  # 2 leaf nodes after the first root
+    node_count = 0
+    size = (50, 50)
+
+    r = wikiPerson(URL, tree_height)
+
+
+    paths, sentences, names = r.get_bfs_paths()
+
+    while i <= pow(2, tree_height + 1):
+        for j in range(1, i, 2):
+            # loop through every odd fraction
+            # i.e. 1/4, 3/4 for loop of size
+            new_shape = None
+
+            # Take the first image retrieved
+            curr_img_path = paths.pop(0)
+            img = Image.open(curr_img_path)
+
+            # resize the image
+            img.thumbnail(size, Image.ANTIALIAS)
+            img = ImageTk.PhotoImage(img)
+
+            # keep a reference so that garbage collector doesn't remove it
+            label = Label(image=img)
+            label.image = img
+
+            # keep track of root element
+            # create the n-th image
+            curr_state = None
+            if node_count == 0:
+                curr_state = "normal"
+            else:
+                curr_state = "hidden"
+
+            img_padding = math.floor(width * (j / i)) - math.floor(width/2)
+
+            new_shape = canvas.create_image(math.floor(width * (j / i)) , height, tags='shape_' + str(node_count),
+                                            image=label.image, anchor=NW, state=curr_state)
+
+            coords = canvas.coords(new_shape)
+            coords[0] += 20
+            coords[1] += 70
+
+            new_name = canvas.create_text(coords[0], coords[1], text=names.pop(0), tags='name_' + str(node_count),
+                                          state=curr_state, width=size[0] + 20, fill="white")
+
+
+            if node_count == 0:
+                root = new_shape
+                root_name = new_name
+
+
+            # tag event to click of image
+            canvas.tag_bind(new_shape, '<Button-1>', click)
+            node_count += 1
+
+        height += 150
+        i *= 2
+
+    # create the tree
+    x = ImageTree(root, 'shape_0', root_name, node_count, canvas, sentences[:])
+    finish = time.perf_counter()
+    print(f'Finished in {round(finish-start, 2)} second(s)')
+    tk.mainloop()
+
+
+URL = "https://en.wikipedia.org/wiki/Bill_Gates"
+init_gui(URL)
